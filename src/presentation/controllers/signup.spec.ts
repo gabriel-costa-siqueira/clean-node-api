62bde820759e4995ad3ea4d70fa07a9337ede6f2
Gabriel Costa Siqueira <gsiqueira1996@gmail.com>
2021-03-30 15:49:10 -0300

    chore: update husky config

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -8,7 +8,7 @@ describe('SignUp Controller', () => {
         name: 'any_name',
         email: "any_email@mail.com",
         password: "any_password",
-        passwordConfirmation: "any_password"
+          passwordConfirmation: "any_password"
       }
     }
     const httpResponse = sut.handle(httpRequest)
