c8247b8f4fdcd1d739817e58991b3b907f47c58e
Gabriel Costa Siqueira <gsiqueira1996@gmail.com>
2021-03-30 15:47:54 -0300

    chore: update husky config

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,7 +3,7 @@ import { SignUpController } from './signup'
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+      const httpRequest = {
       body: {
         name: 'any_name',
         email: "any_email@mail.com",
