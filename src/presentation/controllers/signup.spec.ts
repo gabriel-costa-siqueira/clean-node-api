2ac40459f2dc735f00a6ff9798fd5b97b3bafddb
Gabriel Costa Siqueira <gabriel.siqueira@jbq.global>
2021-03-30 16:18:36 -0300

    chore: update configs

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,11 +7,11 @@ describe('SignUp Controller', () => {
       body: {
         name: 'any_name',
           email: "any_email@mail.com",
-        password: "any_password",
+            password: "any_password",
           passwordConfirmation: "any_password"
       }
     }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
+        const httpResponse = sut.handle(httpRequest)
+                  expect(httpResponse.statusCode).toBe(400)
   })
 })
