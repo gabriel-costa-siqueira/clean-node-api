6ee909641ff23dc5f3ea473125f6dce607ee8180
Gabriel Costa Siqueira <gsiqueira1996@gmail.com>
2021-04-06 03:57:52 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express'
+import { Controller, HttpRequest } from '../../presentation/protocols'
+
+export const adaptRoute = (constroller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await constroller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
